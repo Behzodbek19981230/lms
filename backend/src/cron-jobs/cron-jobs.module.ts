1c5c89f2029956ff7c15bf48efe7a423ca418b11
b_rasulov <b.rasulov@uzinfocom.uz>
2025-09-02 14:53:13 +0500

    feat:payments

--- backend/src/cron-jobs/cron-jobs.module.ts
+++ backend/src/cron-jobs/cron-jobs.module.ts
@@ -6,16 +6,19 @@ import { CronJobsController } from './cron-jobs.controller';
 import { TelegramModule } from '../telegram/telegram.module';
 import { ExamsModule } from '../exams/exams.module';
 import { AttendanceModule } from '../attendance/attendance.module';
+import { PaymentsModule } from '../payments/payments.module';
 import { Exam } from '../exams/entities/exam.entity';
 import { ExamVariant } from '../exams/entities/exam-variant.entity';
+import { Payment } from '../payments/payment.entity';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
-    TypeOrmModule.forFeature([Exam, ExamVariant]),
+    TypeOrmModule.forFeature([Exam, ExamVariant, Payment]),
     TelegramModule,
     ExamsModule,
     AttendanceModule,
+    PaymentsModule,
   ],
   controllers: [CronJobsController],
   providers: [CronJobsService],
