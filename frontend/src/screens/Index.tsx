fe70055513d7d5223840df886fa175a2e550e4ab
b_rasulov <fattoyeva_n@nuu.uz>
2026-01-06 17:52:42 +0500

    fix

--- frontend/src/screens/Index.tsx
+++ frontend/src/screens/Index.tsx
@@ -10,7 +10,7 @@ const Index = () => {
     <div className="min-h-screen">
       <Header />
       <Hero />
-      <Pricing />
+      {/* <Pricing /> */}
       <Features />
       <ContactUs />
       <Footer />
